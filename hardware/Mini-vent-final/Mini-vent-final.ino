9fad016c5c514df0e2f4c4cecec6a5bd0506692d
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2024-02-09 00:00:11 +0530

    arduino code

---
+++ hardware/Mini-vent-final/Mini-vent-final.ino
@@ -0,0 +1,82 @@
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);       // We are using I2C Display
+#define REPORTING_PERIOD_MS     1000
+PulseOximeter pox;                   // pox is the name of oximeter as a variable
+uint32_t tsLastReport = 0;
+int relayPin = 7;  // This is for Uno but if u want to connect with node mcu you can connect with D7 but metion pin GPIO 13 i.e D7 = GPIO 13
+
+void onBeatDetected()
+{
+  Serial.println("Beat!");
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.print("Initializing pulse oximeter..");
+  lcd.begin();
+  lcd.backlight();   // Turn on the blacklight and print a message.
+  lcd.setCursor(2, 0);
+  lcd.print("LIFE SAVIOUR!");
+  delay(2000);
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, HIGH); // Relay High means Off
+
+  if (!pox.begin()) {
+    Serial.println("FAILED");
+
+  } else {
+    Serial.println("SUCCESS");
+  }
+
+
+  pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+  pox.setOnBeatDetectedCallback(onBeatDetected);       // Register a callback for the beat detection
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Make sure to call update as fast as possible
+  pox.update();
+  //float  BPM = pox.getHeartRate();
+  // float SpO2 = pox.getSpO2();
+
+  if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+
+    Serial.print("Heart Beat:");
+    Serial.println(pox.getHeartRate());
+    Serial.print("SpO2:");
+    Serial.println(pox.getSpO2());
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("BPM: ");
+    lcd.print(pox.getHeartRate());
+
+    lcd.setCursor(0, 1);
+    lcd.print("SpO2: ");
+    lcd.print(pox.getSpO2());
+    lcd.print("%");
+
+    if (pox.getSpO2() >= 0 && pox.getSpO2() < 90 ) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Mini-ICU Start!");
+      lcd.setCursor(0, 1);
+    //lcd.print("Spo2 is below 90");
+      lcd.print("SpO2: ");
+      lcd.print(pox.getSpO2());
+      lcd.print("%");
+      Serial.println("Motor Start");
+      digitalWrite(relayPin, LOW); // Relay low means relay On
+
+    }
+    else {
+      digitalWrite(relayPin, HIGH); // Relay low means relay On
+    }
+
+    tsLastReport = millis();
+  }
+}
